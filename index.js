a734357a797133f13e687ca2a9019c752b4cf06a
Nicolas Guillen <nicolas.guillen@wondersauce.com>
2024-08-09 15:11:38 -0400

    Try exporting ffmpeg path from index.js

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-const ffmpegPath = require.resolve("./ffmpegPath/ffmpegPath");
+const os = require("os");
+const path = require("path");
 
-console.log(ffmpegPath);
+function getFFmpegPath() {
+  const platform = os.platform();
+  const arch = os.arch();
+  let ffmpegPath = 'ffmpeg';
+
+  if (platform === "win32") {
+    ffmpegPath = path.resolve(__dirname, "ffmpegPath", "windows/ffmpeg.exe");
+  } else if (platform === "darwin") {
+    ffmpegPath = path.resolve(__dirname, "ffmpegPath", "mac/ffmpeg");
+  } else if (platform === "linux") {
+    if (arch === "x64") {
+      ffmpegPath = path.resolve(__dirname, "ffmpegPath", "linux/amd64/ffmpeg");
+    } else if (arch === "ia32") {
+      ffmpegPath = path.resolve(__dirname, "ffmpegPath", "linux/i686/ffmpeg");
+    } else {
+      throw new Error(`Error: Ffmpeg unsupported architecture: ${arch}`);
+    }
+  } else {
+    throw new Error(`Error: Ffmpeg unsupported platform: ${platform}`);
+  }
+
+  return ffmpegPath;
+}
+
+const ffmpegPath = getFFmpegPath();
+
 module.exports = ffmpegPath;
